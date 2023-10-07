1fadf994fc48457c668a077e639ef51c3c6b78ae
auto login <autologin@ib-410-06.ipa.psati.ru>
2023-10-07 21:12:12 +0800

    2 update

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
 import services
-print(services.take_question())
+import tkinter 
+
+points = 0
+def clicked():  
+    lbl.configure(text = str(question[0]), font=("Arial Bold", 25))
+
+
+
+window = tkinter.Tk()
+window.title("Викторина:)")
+window.geometry('1080x500')
+
+question = services.take_question()
+
+lbl = tkinter.Label(window)
+lbl.pack()
+
+
+btn = tkinter.Button(window, text="Начать", command=clicked)
+btn.pack()
+window.mainloop()
+
+
+print(question[0], '\n')
+
+n = 0
+for elem in list(question[1].keys())[:-1]:
+    n += 1
+    print(n,':', elem)
+
+
+ans = str(input())
+
+try:
+    if question[1][ans] == True:
+        print('Правильно')
+    else:
+        print('Не правильно')
+except KeyError:
+    print('Неправильно')
